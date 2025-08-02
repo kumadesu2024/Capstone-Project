fdd69650c096a5540a0512611430546543bcc467
kumadesu2024 <kurtmilrus@gmail.com>
2025-08-02 22:49:22 +0800

    main

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Cocai',
+        'USER': 'Cocai',
+        'PASSWORD': 'Cocai',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
